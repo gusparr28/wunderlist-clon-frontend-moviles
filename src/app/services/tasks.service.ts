9d89ee32a8a242ed4a9fa6018f508daae90f3c54
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-11-11 03:34:04 -0400

    Final commit

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -3,9 +3,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Subject } from 'rxjs';
 
-import { Task } from '../interfaces/Task';
-import { TaskRes } from '../interfaces/TaskRes';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -28,20 +25,20 @@ export class TasksService {
     });
   }
 
-  public createTask(task: Task): Observable<TaskRes> {
-    return this._http.post<TaskRes>(this._createTaskUrl, {
+  public createTask(task: any): Observable<any> {
+    return this._http.post<any>(this._createTaskUrl, {
       ...task
     }, { headers: this.headers });
   }
 
-  public updateTask(id: string, task: Task): Observable<TaskRes> {
-    return this._http.put<TaskRes>(this._updateTaskUrl + id, {
+  public updateTask(id: string, task: any): Observable<any> {
+    return this._http.put<any>(this._updateTaskUrl + id, {
       ...task
     }, { headers: this.headers });
   }
 
-  public deleteTask(id: string): Observable<TaskRes> {
-    return this._http.delete<TaskRes>(this._deleteTaskUrl + id, { headers: this.headers })
+  public deleteTask(id: string): Observable<any> {
+    return this._http.delete<any>(this._deleteTaskUrl + id, { headers: this.headers })
   }
 
   public changeValue(value: string) {
